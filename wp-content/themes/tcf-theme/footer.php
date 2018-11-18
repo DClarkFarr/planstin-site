b1ab039fc044504253e31d42acd2030146495c57
Daniel C Farr <danielcfarr@gmail.com>
2018-11-17 21:32:34 -0700

    finish footer

--- wp-content/themes/tcf-theme/footer.php
+++ wp-content/themes/tcf-theme/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Wrap -->
 		<section class="section footer-cta bg-theme text-white">
 			<div class="container">
-				<div class="d-flex align-items-center">
+				<div class="d-lg-flex align-items-center">
 					<div>
 						<h3 class="font-weight-normal m-0">
 							ARE YOU READY TO GET STARTED?  REGISTER NOW!
@@ -43,7 +43,7 @@
 									Exclusive portal for employees to choose and enroll for their benefits.
 								</div>
 							</div>
-							<div class="media mb-4">
+							<div class="media">
 								<div class="bg-darker p-3 mr-3 icon text-white">
 									<i class="far fa-user-tie f-15"></i>
 								</div>
@@ -53,8 +53,8 @@
 							</div>
 						</div>
 						<div class="col-lg-3">
-							<h4 class="text-white mb-4">PLANSTIN</h4>
-							<div>
+							<h4 class="text-white mb-4">TAGS</h4>
+							<div class="mb-4">
 								<span class="bg-darker py-1 px-2 mb-2 d-inline-block rounded">Business</span>
 								<span class="bg-darker py-1 px-2 mb-2 d-inline-block rounded">Health</span>
 								<span class="bg-darker py-1 px-2 mb-2 d-inline-block rounded">HR</span>
@@ -63,15 +63,39 @@
 								<span class="bg-darker py-1 px-2 mb-2 d-inline-block rounded">Payroll</span>
 							</div>
 
+							<h4 class="text-white mb-4">LINKS</h4>
+							<ul class="list-unstyled">
+								<li><a class="link" href="">Employer's portal </a></li>
+								<li><a class="link" href="">Employee's portal</a></li>
+								<li><a class="link" href="">Broker's portal</a></li>
+								<li><a class="link" href="">Open Enrollment</a></li>
+								<li><a class="link" href="">Claims Processing</a></li>
+							</ul>
 						</div>
-						<div class="col-lg-3">
-
+						<div class="col-lg-3 get-in-touch">
+							<h4 class="text-white mb-4">GET IN TOUCH</h4>
+							<div class="d-flex mb-3">
+								<div><i class="far fa-fw mr-3 d-inline-block fa-phone"></i></div>
+								<div>(888) 920-7526</div>
+							</div>
+							<div class="d-flex mb-3">
+								<div><i class="far fa-fw mr-3 d-inline-block fa-envelope"></i></div>
+								<div>info@planstin.com</div>
+							</div>
+							<div class="d-flex mb-3">
+								<div><i class="far fa-fw mr-3 d-inline-block fa-map-marker"></i></div>
+								<div>283 W Hilton Dr Suite 1<br>St George, UT 84770</div>
+							</div>
+							<h4 class="text-white my-4">BUSINESS HOURS</h4>
+							<div>
+								Monday – Friday: 8 am to 6 pm MST
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="footer-bottom py-3">
-
+			<div class="footer-bottom bg-darker text-white text-center py-3">
+				All Rights Reserved 2018 Planstin Inc.
 			</div>
 		</footer>
 		<?php wp_footer(); ?>
