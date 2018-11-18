c01cb5914f2cb6c1c9d80d44d783a2d914878f3c
Daniel C Farr <danielcfarr@gmail.com>
2018-11-17 23:57:58 -0700

    services page

---
+++ wp-content/themes/tcf-theme/page-services.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ Template Name: Services Page
+*/
+?>
+
+
+<section class="section-sm bg-gray-light">
+    <div class="container text-center">
+        <h2 class="m-0 text-secondary f-24 text-uppercase">Services</h2>
+    </div>
+</section>
+
+<section class="section services bg-light-alt">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="media mb-5">
+                    <div class="mr-5">
+                        <i class="far fa-fw fa-money-bill f-35 text-theme"></i>
+                    </div>
+                    <div class="media-body pr-5">
+                        <h5 class="mt-0 f-10 text-dark mb-3 font-weight-bold">Full service Payroll</h5>
+                        <span class="f-08">Payroll made easy with a dedicated payroll specialist, all taxes and forms processed, direct deposit payroll, HR Suite and more.  Our services can make payroll deductions and workers comp reporting a breeze so that you can focus on your business.</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="media mb-5">
+                    <div class="mr-5">
+                        <i class="far fa-fw fa-question f-35 text-theme"></i>
+                    </div>
+                    <div class="media-body pr-5">
+                        <h5 class="mt-0 f-10 text-dark mb-3 font-weight-bold">Workers Compensation</h5>
+                        <span class="f-08">We help companies with obtaining and maintaining workers compensation coverage with ease.  Our clients enjoy pay-as-you-go or payroll integrated billing for easy audits.</span>
+                    </div>
+                </div>
+            </div>
+            <div class="w-100"></div>
+            <div class="col-lg-6">
+                <div class="media mb-5">
+                    <div class="mr-5">
+                        <i class="far fa-fw fa-user-tie f-35 text-theme"></i>
+                    </div>
+                    <div class="media-body pr-5">
+                        <h5 class="mt-0 f-10 text-dark mb-3 font-weight-bold">Owner's Payroll</h5>
+                        <span class="f-08">We have a payroll solution specifically designed for business owners and partners.  With the dedicated payroll specialist, we will process all taxes and forms, direct deposit payroll and more.</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="media mb-5">
+                    <div class="mr-5">
+                        <i class="far fa-fw fa-users-class f-35 text-theme"></i>
+                    </div>
+                    <div class="media-body pr-5">
+                        <h5 class="mt-0 f-10 text-dark mb-3 font-weight-bold">HR Suite</h5>
+                        <span class="f-08">Our HR Suite comes with our Full Service Payroll or you can subscribe your business.  The HR Suite is perfect for keep your business in HR compliance using tools, forms, trainings, employee handbooks and a compliance suite.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section process text-center">
+    <div class="container mb-5">
+        <h2 class="f-20 text-dark font-weight-normal">Our Process</h2>
+        <p class="w-lg-50 mx-auto">Getting benefits, payroll and HR services setup has never been easier.  You can pick and choose what options you want.</p>
+    </div>
+    <br>
+    <div class="container mb-5">
+        <div class="row">
+            <div class="col-lg-4">
+                <i class="far fa-id-card f-40 text-theme mb-2"></i>
+                <h5 class="text-dark f-08 mb-2">Register your company</h5>
+                <p class="f-08 text-lightest w-lg-75 mx-auto">Establish your company as a client so we can get to work for you.</p>
+            </div>
+            <div class="col-lg-4">
+                <i class="far fa-question f-40 text-theme mb-2"></i>
+                <h5 class="text-dark f-08 mb-2">Choose your benefits</h5>
+                <p class="f-08 text-lightest w-lg-75 mx-auto">Select what benefits you want to offer to your employees.</p>
+            </div>
+            <div class="col-lg-4">
+                <i class="far fa-check-square f-40 text-theme mb-2"></i>
+                <h5 class="text-dark f-08 mb-2">That's it!</h5>
+                <p class="f-08 text-lightest w-lg-75 mx-auto">Enrollment and ongoing service is now easy and effective.</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container mb-5">
+        <a href="" class="btn btn-dark btn-square">Get Started</a>
+    </div>
+</section>
