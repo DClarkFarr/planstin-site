9fe35c9effb1408f3c155b855ca04c93cf0ee51e
Daniel C Farr <danielcfarr@gmail.com>
2018-11-17 22:44:28 -0700

    about page

---
+++ wp-content/themes/tcf-theme/page-about.php
@@ -0,0 +1,118 @@
+<?php
+/*
+ Template Name: About Page
+*/
+?>
+
+
+<section class="section-sm bg-gray-light">
+    <div class="container text-center">
+        <h2 class="m-0 text-secondary f-24 text-uppercase">About</h2>
+    </div>
+</section>
+
+<section class="about section bg-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <h4 class="f-15 text-theme mb-4 font-weight-bold">About Us</h4>
+                <p class="f-15 text-light">
+                    We are a business administration company specializing in employee benefits, payroll and HR services.
+                </p>
+            </div>
+            <div class="col-lg-7 offset-lg-1">
+                <ul class="list-unstyled">
+                    <li class="media">
+                        <div class="mr-3">
+                            <i class="far fa-book f-20 text-theme"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="mt-0 mb-1 text-darker f-10 font-weight-bold">Our Mission</h5>
+                            <p class="text-light">With our service, technology and experience our mission is to bring simplification, savings and structure to your business.</p>
+                        </div>
+                    </li>
+                    <li class="media my-4">
+                        <div class="mr-3">
+                            <i class="far fa-eye f-20 text-theme"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="mt-0 mb-1 text-darker f-10 font-weight-bold">Our Vision</h5>
+                            <p class="text-light">Our vision is to make running business simple and cost effective.  This way you can focus on growing running your business with peace of mind.</p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <div class="mr-3">
+                            <i class="far fa-bullseye-arrow f-20 text-theme"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="mt-0 mb-1 text-darker f-10 font-weight-bold">Our Goal</h5>
+                            <p class="text-light">Our goal is to help businesses succeed with a true win-win attitude and intuitive solutions.  When you need us most, we will be by your side to solve your business needs.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="section team bg-background">
+    <div class="container text-center mb-5">
+        <h2 class="f-24 font-weight-bold text-darker">Meet the Team</h2>
+        <p>
+            Our team is focused on serving your business with real solutions that make a difference.
+        </p>
+    </div>
+    <div class="container">
+        <div class="card-deck card-deck-lg">
+            <div class="card text-center">
+                <div class="card-body">
+                    <div class="mb-3 mx-3">
+                        <img class="img-fluid rounded-circle" src="/wp-content/uploads/team-nathan.jpg" alt="Nathan Udy">
+                    </div>
+                    <h5 class="card-title text-blue f-08">Nathan Udy</h5>
+                    <p class="card-text f-06 mb-5">Chief Executive Officer</p>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="card-body">
+                    <div class="mb-3 mx-3">
+                        <img class="img-fluid rounded-circle" src="/wp-content/uploads/team-tiffanie.jpg" alt="Nathan Udy">
+                    </div>
+                    <h5 class="card-title text-blue f-08">Tiffanie Moffitt</h5>
+                    <p class="card-text f-06 mb-5">Payroll & Billing Specialist</p>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="card-body">
+                    <div class="mb-3 mx-3">
+                        <img class="img-fluid rounded-circle" src="/wp-content/uploads/team-timothy.jpg" alt="Nathan Udy">
+                    </div>
+                    <h5 class="card-title text-blue f-08">Timothy Hirsch</h5>
+                    <p class="card-text f-06 mb-5">Sales Manager</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-deck card-deck-lg">
+            <div class="card text-center">
+                <div class="card-body">
+                    <div class="mb-3 mx-3">
+                        <img class="img-fluid rounded-circle" src="/wp-content/uploads/team-emily.jpg" alt="Nathan Udy">
+                    </div>
+                    <h5 class="card-title text-blue f-08">Emily Winger</h5>
+                    <p class="card-text f-06 mb-5">Claims Manager</p>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="card-body">
+                    <div class="mb-3 mx-3">
+                        <img class="img-fluid rounded-circle" src="/wp-content/uploads/team-janna.jpg" alt="Nathan Udy">
+                    </div>
+                    <h5 class="card-title text-blue f-08">Janna Perkins </h5>
+                    <p class="card-text f-06 mb-5">Member Services</p>
+                </div>
+            </div>
+            <div class="card invisible"></div>
+        </div>
+    </div>
+</section>
